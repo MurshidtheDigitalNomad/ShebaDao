9146ad40fe90310283f484d3d1cdd6eb57b2cf26
MurshidtheDigitalNomad <murshidulhaqueahmed@gmail.com>
2025-11-04 02:52:29 +0600

    a commit

--- client/src/routes/startingpage.route.tsx
+++ client/src/routes/startingpage.route.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoadingAnimation from "../Components/StartingPage/LoadingAnimation";
 import StartingForm from "../Components/StartingPage/StartingForm";
