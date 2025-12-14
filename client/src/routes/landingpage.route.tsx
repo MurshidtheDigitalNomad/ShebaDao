46530f469b0d303004a96abaf44fde8e2b16f868
MurshidtheDigitalNomad <murshidulhaqueahmed@gmail.com>
2025-12-15 04:05:48 +0600

    Updated Hero

--- client/src/routes/landingpage.route.tsx
+++ client/src/routes/landingpage.route.tsx
@@ -2,7 +2,6 @@ import Navbar from "../Components/Landing Page/Navbar";
 import Hero from "../Components/Landing Page/Hero";
 import Features from "../Components/Landing Page/Features";
 import HowItWorks from "../Components/Landing Page/HowItWorks";
-import MeetTheTeam from "../Components/Landing Page/MeetTeam";
 import VisionQuote from "../Components/Landing Page/VisionQuote";
 import Footer from "../Components/Landing Page/Footer";
 
